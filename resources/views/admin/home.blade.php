e555519baf63c497a4cbab00539fad4cb6f0dd38
brreynaldi <brreynaldi@gmail.com>
2025-08-07 16:35:20 +0700

    2nd

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -17,7 +17,7 @@
         <!-- Content Row -->
         <div class="row">
 
-            <!-- List Warga Card Example -->
+            <!-- List Alternatif Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
                 <a href="{{ route('alternatif.index') }}">
                     <div class="card border-left-primary shadow h-100 py-2">
@@ -25,7 +25,7 @@
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Jumlah Warga</div>
+                                        Jumlah Alternatif</div>
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $alternatif }}</div>
                                 </div>
                                 <div class="col-auto">
--- resources/views/admin/report/index.blade.php
+++ resources/views/admin/report/index.blade.php
@@ -48,14 +48,14 @@
                 </a>
             </div>
 
-            <!-- warga Card Example -->
+            <!-- Alternatif Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
                 <a href="{{ URL::to('download-alternatif-pdf') }}" target="_blank">
                     <div class="card border-left-danger shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Data Warga/Alternative</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Data Alternative</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-users fa-2x text-gray-300"></i>
